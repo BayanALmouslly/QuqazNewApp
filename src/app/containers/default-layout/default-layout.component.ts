172c66617f68529e5089f742c69b7c0cb1755750
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-06-28 17:09:52 +0300

    first commit

---
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { navItems } from '../../_nav';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './default-layout.component.html'
+})
+export class DefaultLayoutComponent {
+  public sidebarMinimized = false;
+  public navItems = navItems;
+  constructor(private router: Router) { }
+  toggleMinimize(e) {
+    this.sidebarMinimized = e;
+  }
+  logout() {
+    this.router.navigate(['/login'])
+  }
+}
