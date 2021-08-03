bef00d5563464b6244b20f9b4f7c7fa4d72bd4cd
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-08-04 01:52:25 +0300

    notfiaction

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -91,8 +91,14 @@ export class DefaultLayoutComponent implements OnInit {
     this.paging.Page = event.page
     this.getNotfiaction()
   }
-  @HostListener('scroll', ['$event'])
+  @HostListener("window:scroll", ["$event"])
   scrollHandler(event) {
-    console.log(event);
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    let max = document.documentElement.scrollHeight;
+    // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+     if(pos == max )   {
+    console.log(event)
+     }
+
   }
 }
