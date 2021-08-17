785d8ea7d8a03933f2b2cb1f70ec44dac0fe19f1
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-08-17 12:29:35 +0300

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: "https://backend.quqaz.com/",
+  // baseUrl: "https://backend.quqaz.com/",
+  baseUrl: "http://mohammedhatem-001-site4.itempurl.com/",
 
 };
