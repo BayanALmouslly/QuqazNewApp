e78ed696e0e361829d9512acfc226dd6fb6b7da2
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-08-30 13:07:41 +0300

    env

--- src/app/views/orders/print-order/print-order.component.ts
+++ src/app/views/orders/print-order/print-order.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { environment } from '../../../../environments/environment.prod';
 import { UserLogin } from '../../auth/userlogin.model';
 
 @Component({
@@ -13,9 +14,14 @@ export class PrintOrderComponent implements OnInit {
   orderTypes: string[] = []
   counts: number[] = []
   client: UserLogin = JSON.parse(localStorage.getItem('kokazClient'))
+  agentPhone = environment.companyPhones[1]
+  address = environment.Address
+  whatsapp = environment.whatsapp
+  instgram = environment.instgram
+  facebook = environment.Facebook
   ngOnInit(): void {
     // this.order.recipientPhones = this.order.recipientPhones.split(',')
-    if(  this.order&&this.order.orderItems&&this.order.orderItems.length!=0){
+    if (this.order && this.order.orderItems && this.order.orderItems.length != 0) {
       this.orderTypes = this.order.orderItems.map(o => o.orderTpye.name)
       this.counts = this.order.orderItems.map(o => o.count)
     }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,14 @@
 
 export const environment = {
   production: false,
-  baseUrl:"https://localhost:44333/",
-   appNameAR:"شركة القوقز للتوصيل السريع",
-  appNameEN:"Quqaz"
+  baseUrl: "https://localhost:44333/",
+  // Quqaz
+
+  appNameAR: "شركة القوقز للتوصيل السريع",
+  appNameEN: "Quqaz",
+
+  // Golden Wings
+
   // appNameAR:"شركة الأجنحة الذهبية",
   // appNameEN:"Golden Wings",
 };
