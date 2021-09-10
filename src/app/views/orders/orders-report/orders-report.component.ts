78c383b299696b808ea50e8a5d95f43efe8b5672
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-09-10 05:05:41 +0300

    notficaton

--- src/app/views/orders/orders-report/orders-report.component.ts
+++ src/app/views/orders/orders-report/orders-report.component.ts
@@ -52,28 +52,28 @@ export class OrdersReportComponent implements OnInit {
     else return
   }
   clientCalc = 0
-  payForCleint(element): number {
+  // payForCleint(element): number {
 
-    if (!element.isClientDiliverdMoney) {
-      if (element.orderplaced.id == 5)
-        return 0;
-      return element.cost - element.deliveryCost;
+  //   if (!element.isClientDiliverdMoney) {
+  //     if (element.orderplaced.id == 5)
+  //       return 0;
+  //     return element.cost - element.deliveryCost;
 
-    }
-    else {
+  //   }
+  //   else {
 
-      //مرتجع كلي
-      if (element.orderplaced.id == 5)
-        return element.deliveryCost - element.oldCost;
-      //مرفوض
-      else if (element.orderplaced.id == 7)
-        return (-element.oldCost);
-      //مرتجع جزئي
-      else if (element.orderplaced.id == 6)
-        return element.cost - element.oldCost;
-    }
+  //     //مرتجع كلي
+  //     if (element.orderplaced.id == 5)
+  //       return element.deliveryCost - element.oldCost;
+  //     //مرفوض
+  //     else if (element.orderplaced.id == 7)
+  //       return (-element.oldCost);
+  //     //مرتجع جزئي
+  //     else if (element.orderplaced.id == 6)
+  //       return element.cost - element.oldCost;
+  //   }
 
-  }
+  // }
   deliveryCostCount=0
   count=0
   reports: any[] = []
@@ -86,36 +86,37 @@ export class OrdersReportComponent implements OnInit {
       this.orders.forEach(o => {
         this.count += o.cost
         this.deliveryCostCount += o.deliveryCost
-        if (!o.isClientDiliverdMoney) {
-          if (o.orderplaced.id == OrderplacedEnum.CompletelyReturned) {
-            this.clientCalc += 0
-            return 0;
-          }
-          else if (o.orderplaced.id == OrderplacedEnum.Unacceptable) {
-            this.clientCalc += o.deliveryCost
-            return o.deliveryCost;
-          }
-          this.clientCalc += o.cost - o.deliveryCost
-          return o.cost - o.deliveryCost;
+        this.clientCalc+=o.payForClient
+        // if (!o.isClientDiliverdMoney) {
+        //   if (o.orderplaced.id == OrderplacedEnum.CompletelyReturned) {
+        //     this.clientCalc += 0
+        //     return 0;
+        //   }
+        //   else if (o.orderplaced.id == OrderplacedEnum.Unacceptable) {
+        //     this.clientCalc += o.deliveryCost
+        //     return o.deliveryCost;
+        //   }
+        //   this.clientCalc += o.cost - o.deliveryCost
+        //   return o.cost - o.deliveryCost;
 
-        }
-        else {
-          //مرتجع كلي
-          if (o.orderplaced.id == OrderplacedEnum.CompletelyReturned) {
-            this.clientCalc += o.deliveryCost - o.cost
-            return o.deliveryCost - o.cost;
-          }
-          //مرفوض
-          else if (o.orderplaced.id == OrderplacedEnum.Unacceptable) {
-            this.clientCalc += (-o.cost)
-            return (-o.cost);
-          }
-          //مرتجع جزئي
-          else if (o.orderplaced.id == OrderplacedEnum.PartialReturned) {
-            this.clientCalc += o.cost - o.oldCost;
-            return o.cost - o.oldCost;
-          }
-        }
+        // }
+        // else {
+        //   //مرتجع كلي
+        //   if (o.orderplaced.id == OrderplacedEnum.CompletelyReturned) {
+        //     this.clientCalc += o.deliveryCost - o.cost
+        //     return o.deliveryCost - o.cost;
+        //   }
+        //   //مرفوض
+        //   else if (o.orderplaced.id == OrderplacedEnum.Unacceptable) {
+        //     this.clientCalc += (-o.cost)
+        //     return (-o.cost);
+        //   }
+        //   //مرتجع جزئي
+        //   else if (o.orderplaced.id == OrderplacedEnum.PartialReturned) {
+        //     this.clientCalc += o.cost - o.oldCost;
+        //     return o.cost - o.oldCost;
+        //   }
+        // }
       })
 
     return this.count
