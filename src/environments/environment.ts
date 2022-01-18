fa467e475a184a4e7629f5cba2b58d57a48cc314
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-01-18 13:34:38 +0200

    new branch golden-w

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,6 @@
 export const environment = {
   production: false,
   baseUrl: "https://localhost:44333/",
-  // Quqaz
-
-  appNameAR: "شركة القوقز للتوصيل السريع",
-  appNameEN: "Quqaz",
-
-  // Golden Wings
-
-  // appNameAR:"شركة الأجنحة الذهبية",
-  // appNameEN:"Golden Wings",
+  appNameAR:"شركة الأجنحة الذهبية للتوصيل",
+  appNameEN:"Golden Wings",
 };
