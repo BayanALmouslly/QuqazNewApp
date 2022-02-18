559d8b247ba57923a4761d833058dc21a9c8377c
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-02-18 22:00:59 +0200

    golden branch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: "http://mohammedhatem-001-site4.itempurl.com/",
+  baseUrl: "http://goldenwing-001-site2.etempurl.com/",
   appNameAR: "شركة الأجنحة الذهبية للتوصيل",
   appNameEN: "Golden Wings",
   Address: "حي العدل قرب بانزيخانة لؤلؤة العدل  مجاور افران الوردة",
